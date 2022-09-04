d40587b2f7a5e24c897744a87e45a44f08aa8132
Karina Valens <karyvalens@yahoo.com>
2022-09-04 22:33:08 +0200

    capitalization

---
+++ capitalization3.js
@@ -0,0 +1,4 @@
+const nameSt = "Frederica";
+
+const newName = (nameSt.substring(0, 2).toLowerCase()) + (nameSt.substring(2, 3).toUpperCase()) + (nameSt.substring(3).toLowerCase());
+console.log(newName);
