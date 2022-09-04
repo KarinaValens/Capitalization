99e340412ecc0f334e6c936e2c046a776225f314
Karina Valens <karyvalens@yahoo.com>
2022-09-04 22:42:31 +0200

    real capitalization

--- capitalization.js
+++ capitalization.js
@@ -1,3 +1,10 @@
+//Real Capitalization
+const name1 = "albusternem";
+const captName = name1.substring(0, 1).toUpperCase() + name1.substring(1).toLowerCase();
+console.log(captName);
+
+
+//3caPitalization
 const nameSt = "Frederica";
 
 const newName = (nameSt.substring(0, 2).toLowerCase()) + (nameSt.substring(2, 3).toUpperCase()) + (nameSt.substring(3).toLowerCase());
