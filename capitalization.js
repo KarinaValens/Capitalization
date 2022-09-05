324387099485b9fe7d48915f25d57124d2dd64ce
Karina Valens <karyvalens@yahoo.com>
2022-09-05 15:20:26 +0200

    put code inside a function

--- capitalization.js
+++ capitalization.js
@@ -1,11 +1,18 @@
-
 //Real Capitalization
-const name1 = "albusternem";
-const captName = name1.substring(0, 1).toUpperCase() + name1.substring(1).toLowerCase();
-console.log(captName);
+
+const str = "PEter";
+
+capitalize(str); // it is necesary to call the function in order the console.log work
+
+function capitalize(str) {
+    newStr = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    console.log(newStr);
+}
+
+console.log();
 
 //3caPitalization
 const nameSt = "Frederica";
 
 const newName = (nameSt.substring(0, 2).toLowerCase()) + (nameSt.substring(2, 3).toUpperCase()) + (nameSt.substring(3).toLowerCase());
-console.log(newName);
+//console.log(newName);
