c81a85ac797bcee9505c8a5c09d244a33194d2fa
Karina Valens <karyvalens@yahoo.com>
2022-09-07 11:03:01 +0200

    with a return inside the function

--- capitalization.js
+++ capitalization.js
@@ -2,17 +2,14 @@
 
 const str = "PEter";
 
-capitalize(str); // it is necesary to call the function in order the console.log work
+// it is necesary to call the function in order the console.log work
 
 function capitalize(str) {
-    newStr = str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-    console.log(newStr);
+    return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 }
-
-console.log();
+console.log(capitalize(str));
 
 //3caPitalization
 const nameSt = "Frederica";
-
 const newName = (nameSt.substring(0, 2).toLowerCase()) + (nameSt.substring(2, 3).toUpperCase()) + (nameSt.substring(3).toLowerCase());
 //console.log(newName);
